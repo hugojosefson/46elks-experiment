65565ffcbab2a8a4dbcd6134f98169693749b2c1
Hugo Josefson <hugo@josefson.org>
2015-12-14 23:02:16 +0100

    Split into three webapps.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-import {PORT} from './lib/config';
-import app from './lib/webapp';
+import {PORT, ELKS_PORT, ADMIN_PORT} from './lib/config';
+import publicApp from './lib/public-app';
+import elksApi from './lib/elks-api';
+import adminApi from './lib/admin-api';
 
-app.listen(PORT, err => {
+[
+    [publicApp, PORT, 'Public app'],
+    [elksApi, ELKS_PORT, 'Elks API'],
+    [adminApi, ADMIN_PORT, 'Admin API']
+].forEach(([app, port, name]) => app.listen(port, err => {
     if (err) {
         console.error(err);
     } else {
-        console.log('Listening on port ' + PORT);
+        console.log(`${name} listening on port ${port}`);
     }
-});
+}));
