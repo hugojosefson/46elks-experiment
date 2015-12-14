0dfa31b879c8874b965d11eae38a6d935e66e7d9
Hugo Josefson <hugo@josefson.org>
2015-12-14 23:29:59 +0100

    Respond 405 Method Not Allowed correctly.

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import publicApp from './lib/public-app';
 import elksApi from './lib/elks-api';
 import adminApi from './lib/admin-api';
 
-[
-    [publicApp, PORT, 'Public app'],
-    [elksApi, ELKS_PORT, 'Elks API'],
-    [adminApi, ADMIN_PORT, 'Admin API']
-].forEach(([app, port, name]) => app.listen(port, err => {
+const listenListener = (name, port) => err => {
     if (err) {
         console.error(err);
     } else {
         console.log(`${name} listening on port ${port}`);
     }
-}));
+};
+
+[
+    [publicApp, PORT, 'Public app'],
+    [elksApi, ELKS_PORT, 'Elks API'],
+    [adminApi, ADMIN_PORT, 'Admin API']
+].forEach(([app, port, name]) => app.listen(port, listenListener(name, port)));
