4704c8239f6ec6c1ae4988f52bf3a5a75abadf8f
Hugo Josefson <hugo@josefson.org>
2016-01-06 19:56:18 +0100

    Respond 204 on receiving recorded call.

--- src/elks-api/routes/voice/recorded-post.js
+++ src/elks-api/routes/voice/recorded-post.js
@@ -1,9 +1,9 @@
-import {respond200} from 'express-respond-simple';
+import {respond204} from 'express-respond-simple';
 import bodyParser from 'body-parser';
 import log from '../../../log';
 
 export default [
     bodyParser.json(),
     log,
-    respond200()
+    respond204()
 ];
